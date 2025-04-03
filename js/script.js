f2a1094036e75ebad5834c50ab82c8d5ac707edb
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-03 14:22:16 +0200

    [UPD] started js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//andiamo a recuperare gli elementi dal dom
+const EmailList = document.getElementById("email-list")
+//andiamo a dichiarare le variabili
+const arrayMail = []
+const endpointi = "https:flynn.boolean.careers/exercises/api/random/mail"
