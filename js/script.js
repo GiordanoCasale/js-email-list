5acf6a2759d7721a64bd9d2b501a6ecd4491efd2
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-03 14:57:25 +0200

    [UPD] variable declared

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //andiamo a recuperare gli elementi dal dom
 const EmailList = document.getElementById("email-list")
 //andiamo a dichiarare le variabili
-const arrayMail = []
-const endpointi = "https:flynn.boolean.careers/exercises/api/random/mail"
+const arrayMail = [];
+let items = 10;
+const endpoint = `https:flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
