ea580fb34b3c5404ec3d798b3b96d9fc2f14ad53
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-03 14:59:13 +0200

    [UPD] for created

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ const EmailList = document.getElementById("email-list")
 const arrayMail = [];
 let items = 10;
 const endpoint = `https:flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
+//vado a creare un for per ciclare l'array
+for (let i = 0; i < items; i++) {
+    
