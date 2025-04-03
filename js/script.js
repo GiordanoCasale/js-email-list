a36a9ebc3344b315ddffccbe46eebf38f2dbb1ad
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-03 14:59:38 +0200

    [DONE] exercise completed

--- js/script.js
+++ js/script.js
@@ -6,4 +6,14 @@ let items = 10;
 const endpoint = `https:flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
 //vado a creare un for per ciclare l'array
 for (let i = 0; i < items; i++) {
-    
+    axios.get(endpoint).then((response) => {
+        const mail = response.data.response;
+        console.log(mail);
+        
+        // Aggiungo l'email all'array
+        arrayMail.push(mail);
+
+        // Aggiungo l'email alla lista HTML
+        EmailList.innerHTML += `<li class="list-group-item">${mail}</li>`;
+    });
+}
