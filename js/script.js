4ce57bd516e7cb8b64dde56bb1b41c50ae788b80
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-07 16:42:28 +0200

    [FIXED] fixed problem

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const EmailList = document.getElementById("email-list")
 //andiamo a dichiarare le variabili
 const arrayMail = [];
 let items = 10;
-const endpoint = `https:flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
 //vado a creare un for per ciclare l'array
 for (let i = 0; i < items; i++) {
     axios.get(endpoint).then((response) => {
